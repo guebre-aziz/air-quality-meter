7d97d112764c82ca2a4d76182281a9b0d5a40afc
guebre_aziz_ <guebreaziz@gmail.com>
2021-08-25 00:56:28 +0200

    fetch error handling complete

--- src/index.js
+++ src/index.js
@@ -3,29 +3,77 @@ import "./style.css";
 // const img = new Image();
 // img.src = landscape;
 
-const title = document.createElement("h3");
-title.textContent = "Pullution index";
-document.body.append(title);
-title.classList.add("hello");
+const apiKey = process.env.API_KEY;
+let cityData;
 
-
-
-
-
-
-let apiKey = process.env.API_KEY;
-let form = document.forms.cityFieldForm;
-let inputCity = form.elements.cityInput;
-let citta = "milano";
-
+// search city from input field
 async function getDataFromInput(cityName) {
   try {
     let response = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`);
-    let cityData = await response.text();
-    document.body.append(cityData);
+    let cityDataTemporary = await response.json();
+    if(cityDataTemporary.status == "error") throw cityDataTemporary;
+    cityData = cityDataTemporary;
+    textCity.textContent = cityData.data.city.name;
+    // console.log(cityData);
+    // textCity.textContent = cityData.data.city.name;
   } catch (err) {
-    alert("Opsss: " + err.message);
+
+    if(err.status == "error" && err.data == "Over quota") {
+      alert("Server sovvracarico di richieste, attendere un istante e riprovare");
+    }
+    else if(err.status == "error" && err.data == "Invalid key") {
+      alert("Chiave API invalida, contattare amministratore del sito web");
+    }
+    else if(err.status == "error" && err.data == "Unknown station") {
+      alert("Città sconosciuta, prego inserire un altro");
+    }
+    else {
+      alert("Si è verificato un errore: " + err.name + ": " + err.message);
+    }
   }
 };
 
-getDataFromInput("berlin");
+cityInput.addEventListener("keydown", (event) => {
+  // setTimeout(() => getDataFromInput(event.target.value), 1500);
+  if (event.keyCode === 13) {
+    getDataFromInput(event.target.value);
+  }
+});
+
+// richearch from gelocalisation
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+async function getPosAppruved(pos) {
+  try {
+    let crd = pos.coords;
+    let response = await fetch(`https://api.waqi.info/feed/geo:${crd.latitude};${crd.longitude}/?token=${apiKey}`);
+    let cityDataTemporary = await response.json();
+    if(cityDataTemporary.status == "error") throw cityDataTemporary;
+    cityData = cityDataTemporary;
+    textCity.textContent = cityData.data.city.name;
+    // console.log(`More or less ${crd.accuracy} meters.`);
+  } catch (err) {
+
+    if(err.status == "error" && err.data == "Over quota") {
+      alert("Server sovvracarico di richieste, attendere un istante e riprovare");
+    }
+    else if(err.status == "error" && err.data == "Invalid key") {
+      alert("Chiave API invalida, contattare amministratore del sito web");
+    }
+    else {
+      alert("Si è verificato un errore: " + err.name + ": " + err.message);
+    }
+  }
+};
+
+function getPosDenied(err) {
+  alert("Errore accesso a posizione attuale. Prego riprovare.");
+}
+
+getPosButton.onclick = () => navigator.geolocation.getCurrentPosition(getPosAppruved, getPosDenied, options);
+// main
